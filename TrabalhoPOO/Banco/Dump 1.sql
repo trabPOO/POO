c307753afc63753d9adcfce45e4b7dde2b0ffa3a
JeffersonMoran <jeffersonmoran79@gmail.com>
2018-05-15 00:48:24 -0300

    banco certo

--- TrabalhoPOO/Banco/Dump 1.sql
+++ TrabalhoPOO/Banco/Dump 1.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `veterinaria` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `veterinaria`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: veterinaria
 -- ------------------------------------------------------
--- Server version	5.7.19-log
+-- Server version	5.7.17-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,16 +25,19 @@ DROP TABLE IF EXISTS `animal`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `animal` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_animal` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
   `idade` int(11) DEFAULT NULL,
   `raca` varchar(45) DEFAULT NULL,
   `porte` varchar(45) DEFAULT NULL,
-  `especie` varchar(45) DEFAULT NULL,
-  `dono_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `dono_id_idx` (`dono_id`),
-  CONSTRAINT `dono_id` FOREIGN KEY (`dono_id`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `deleted` int(11) DEFAULT '0',
+  `id_cliente` int(11) DEFAULT NULL,
+  `id_especie` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_animal`),
+  KEY `id_cliente_idx` (`id_cliente`),
+  KEY `id_especie_idx` (`id_especie`),
+  CONSTRAINT `id_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_especie` FOREIGN KEY (`id_especie`) REFERENCES `especie` (`id_especie`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -55,12 +58,13 @@ DROP TABLE IF EXISTS `cliente`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
   `cpf` varchar(45) DEFAULT NULL,
   `telefone` double DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `deleted` int(11) DEFAULT '0',
+  PRIMARY KEY (`id_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -81,7 +85,7 @@ DROP TABLE IF EXISTS `consulta`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `consulta` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_consulta` int(11) NOT NULL AUTO_INCREMENT,
   `data` varchar(45) DEFAULT NULL,
   `valor` double DEFAULT NULL,
   `local` varchar(45) DEFAULT NULL,
@@ -89,10 +93,17 @@ CREATE TABLE `consulta` (
   `sintoma` varchar(45) DEFAULT NULL,
   `diagnostico` varchar(45) DEFAULT NULL,
   `status` varchar(45) DEFAULT NULL,
-  `animal_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `animal_id` (`animal_id`),
-  CONSTRAINT `animal_id` FOREIGN KEY (`animal_id`) REFERENCES `animal` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `deleted` int(11) DEFAULT '0',
+  `id_veterinario` int(11) DEFAULT NULL,
+  `id_animal` int(11) DEFAULT NULL,
+  `id_secretaria` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_consulta`),
+  KEY `id_veterinario_idx` (`id_veterinario`),
+  KEY `id_animal_idx` (`id_animal`),
+  KEY `id_secretaria_idx` (`id_secretaria`),
+  CONSTRAINT `id_animal` FOREIGN KEY (`id_animal`) REFERENCES `animal` (`id_animal`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_secretaria` FOREIGN KEY (`id_secretaria`) REFERENCES `secretaria` (`id_secretaria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_veterinario` FOREIGN KEY (`id_veterinario`) REFERENCES `veterinario` (`id_veterinario`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -106,6 +117,29 @@ LOCK TABLES `consulta` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `especie`
+--
+
+DROP TABLE IF EXISTS `especie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `especie` (
+  `id_especie` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_especie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `especie`
+--
+
+LOCK TABLES `especie` WRITE;
+/*!40000 ALTER TABLE `especie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `especie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `exame`
 --
 
@@ -113,17 +147,18 @@ DROP TABLE IF EXISTS `exame`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `exame` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_exame` int(11) NOT NULL,
   `data` varchar(45) DEFAULT NULL,
   `tipoExame` varchar(45) DEFAULT NULL,
   `horario` varchar(45) DEFAULT NULL,
   `local` varchar(45) DEFAULT NULL,
   `resultado` varchar(45) DEFAULT NULL,
   `status` varchar(45) DEFAULT NULL,
-  `animal_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `animal_id_idx` (`animal_id`),
-  CONSTRAINT `ex_animal_id` FOREIGN KEY (`animal_id`) REFERENCES `animal` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `id_consulta` int(11) DEFAULT NULL,
+  `deleted` int(11) DEFAULT '0',
+  PRIMARY KEY (`id_exame`),
+  KEY `id_consulta_idx` (`id_consulta`),
+  CONSTRAINT `id_consulta` FOREIGN KEY (`id_consulta`) REFERENCES `consulta` (`id_consulta`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -137,6 +172,32 @@ LOCK TABLES `exame` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `secretaria`
+--
+
+DROP TABLE IF EXISTS `secretaria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `secretaria` (
+  `id_secretaria` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `ramal` int(11) DEFAULT NULL,
+  `cpf` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_secretaria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `secretaria`
+--
+
+LOCK TABLES `secretaria` WRITE;
+/*!40000 ALTER TABLE `secretaria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `secretaria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `veterinario`
 --
 
@@ -144,13 +205,14 @@ DROP TABLE IF EXISTS `veterinario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `veterinario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_veterinario` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
   `crm` varchar(45) DEFAULT NULL,
   `especializacao` varchar(45) DEFAULT NULL,
   `cpf` varchar(45) DEFAULT NULL,
   `salario` double DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `deleted` int(11) DEFAULT '0',
+  PRIMARY KEY (`id_veterinario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -180,4 +242,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-15 23:40:33
+-- Dump completed on 2018-05-14 20:34:53
